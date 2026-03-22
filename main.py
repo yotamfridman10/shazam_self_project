1404ef836718b48a0a96693081efa225f6cc7194
yotamfridman10 <yotamfridman10@gmail.com>
2026-03-22 13:55:09 +0200

    describe what you did

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import psycopg2
 from db import init_db
-from song_analysis import open_song_wav 
-from song_analysis import analyze_new_song
+from song_analysis import analyze_new_song, analyze_query_song
 
 conn = psycopg2.connect(
     dbname="songs_fingerprints_db", 
@@ -14,6 +13,13 @@ init_db(conn)
 
 def main():
     analyze_new_song(conn.cursor(), conn, "song1.wav")
+    analyze_new_song(conn.cursor(), conn, "song2.wav")
+    analyze_new_song(conn.cursor(), conn, "song3.wav")
+    analyze_new_song(conn.cursor(), conn, "song4.wav")
+    analyze_new_song(conn.cursor(), conn, "song5.wav")
+
+    best_match_song = analyze_query_song("query_song1.wav", conn.cursor())
+
 
 
 if __name__ == "__main__":
